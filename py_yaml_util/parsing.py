14cf2f92ebec70c316637cb06329172281506b9c
nvallepalli <prasanth.vallepalli@gmail.com>
2017-08-13 21:04:28 -0500

    initial commit

--- py_yaml_util/parsing.py
+++ py_yaml_util/parsing.py
@@ -3,7 +3,6 @@ import requests
 import os
 
 def main():
-
     yaml_file('https://raw.githubusercontent.com/unitedstates/congress-legislators/master/executive.yaml')
     get_birthday("Thomas", "Jefferson")
 
@@ -16,6 +15,7 @@ def yaml_file(url):
         for chunk in r.iter_content():
             fd.write(chunk)
 
+
 def get_birthday(fname,lname):
 
     with open(path1, 'r') as f:
@@ -25,12 +25,15 @@ def get_birthday(fname,lname):
                 txt = name1['bio']['birthday']
                 print(txt)
 
+
 def _get_file_path():
     path = os.path.dirname(os.path.abspath(__file__))
     split_path = os.path.split(path)
     return os.path.join(split_path[0], 'templates')
 
+
 path1 = os.path.join(_get_file_path(), 'sample.yml')
+
 
 if __name__ == '__main__':
     main()
